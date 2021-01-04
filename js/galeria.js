c93fab74c56c5b51f5ea47f0b87ee458a9efae35
samiri32 <samirsimnica32@gmail.com>
2021-01-04 14:06:51 +0100

    added navbar te galeria edhe pak ndryshime css

--- js/galeria.js
+++ js/galeria.js
@@ -1,49 +1,39 @@
-var slideIndex = 1;
-showSlides(1);
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
 
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName('Sllajdet');
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-
-
-}
-
-var modal = document.getElementById("myModal");
-
-var img = document.getElementsByClassName("fototmodal");
-var modalImg = document.getElementById("img");
-
-var showModal = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-
-}
-for (var i = 0; i < img.length; i++) {
-  img[i].addEventListener('click', showModal);
-}
-
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function () {
-  modal.style.display = "none";
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
