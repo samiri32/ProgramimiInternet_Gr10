8e39017e034fdd909a279d69ed8265f5149b214d
AdnitGashi <66799426+AdnitGashi@users.noreply.github.com>
2021-01-11 18:42:56 +0100

    urime president i rep se kosoves

--- js/Kontakti.js
+++ js/Kontakti.js
@@ -1,1 +1,15 @@
-alert("shabot");
+$("h1.formularii").add("formulari");
+
+$(".harta").hide();
+$(".largoHarten").hide();
+$(".largoVideon").hide();
+$(".Video").hide();
+$(".shfaqeHarten").click(function(){
+$(".shfaqeHarten").hide();
+$(".harta").slideDown();
+$(".largoHarten").slideDown();
+  }
+);
+$(".shfaqVideon").click(function(){
+  $(".Video").slideDown();
+});
