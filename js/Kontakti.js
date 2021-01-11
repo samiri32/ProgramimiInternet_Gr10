4d0bae8dada3c5ec44ec8f4e6abbbe8d6cdde49f
AdnitGashi <66799426+AdnitGashi@users.noreply.github.com>
2021-01-11 19:26:19 +0100

    ad

--- js/Kontakti.js
+++ js/Kontakti.js
@@ -1,15 +1,1 @@
 $("h1.formularii").add("formulari");
-
-$(".harta").hide();
-$(".largoHarten").hide();
-$(".largoVideon").hide();
-$(".Video").hide();
-$(".shfaqeHarten").click(function(){
-$(".shfaqeHarten").hide();
-$(".harta").slideDown();
-$(".largoHarten").slideDown();
-  }
-);
-$(".shfaqVideon").click(function(){
-  $(".Video").slideDown();
-});
